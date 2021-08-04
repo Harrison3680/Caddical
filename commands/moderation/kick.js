8af918a0f141cb21b7a40fe5320878863397ec4a
DinoTheDev <77805841+DinoTheDevOfficial@users.noreply.github.com>
2021-08-04 13:52:30 -0700

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -1,6 +1,7 @@
-module.exports = ({
+module.exports = {
 name:"kick",
-code:`$title[$username[$authorID] kicked $username[$findUser[$message[1]] for $message[2]]
-$kick[$findUser[$message[1]];$message[2]]`
+code:`
+$description[$username[$authorID] kicked $username[$findUser[$message[1]] for $filterMessage[$message;$message[1]]
+$kick[$findUser[$message[1]];$filterMessage[$message;$message[1]]]`
 `  
-})
+}

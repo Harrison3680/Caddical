14cc89070f4cf74db4888e18e8295343688b8ac3
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-04 14:19:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
+global.config = require("./config.json")
+const Dlang = require('discordbot-script')
+const bot = new Dlang({
+  token: config.TOKEN,
+  prefix: [""]
+})
+bot.MessageEvent()
+
+const fs = require('fs');
+const folders = fs.readdirSync("./commands/")
+
+for (const files of folders) {
+    const folder = fs.readdirSync(`./commands/${files}/`).filter(file => file.endsWith(".js"))
+
+    for (const commands of folder) {
+        const command = require(`./commands/${files}/${commands}`)
+        bot.Command(command);
+        console.log(`Loaded: ${command.name} | ${files}`);
+    }
+}
+
+bot.Variables({
+  cash:"0",
+  bank:"0"
+})
+
+bot.Status({
+    0: {
+        description:"with the developers", 
+        type: "PLAYING" 
+    }, 
+    1: {
+        description: "over $guildCount Guilds", 
+        type: "WATCHING" 
+    }
+}, 12000)
+ 
+bot.ReadyCommand({
+name: "",
+code: `
+$client[tag] has been started at $client[readyat]!
+$exec[git pull origin main]`
+
+})
 

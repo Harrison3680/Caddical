07eeff8df877b80c07b0d9f22433f7ea2e5d0e4b
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-04 22:57:52 -0500

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -3,6 +3,7 @@ name:"kick",
 code:`$kick[$findUser[$message[1]];$message[2]]
 $description[$username[$authorID] kicked $username[$findUser[$message[1]] for $message[2]].]
 $onlyBotPerms[kick;i need the {perms} permission to be able to do this.]
-`
+$onlyPerms[kick;You must have the {perms} permission to use this command.]
+$onlyIf[$messsage[1]!=;Please mention or give an id of someone to kick]
 `
 })

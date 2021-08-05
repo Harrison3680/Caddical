120a832538714c3fdc624585563a733a2ef8eda8
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-04 22:58:26 -0500

    Update ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -3,5 +3,7 @@ name:"ban",
 code:`$ban[$findUser[$message[1]];$message[2]]
 $description[$username[$authorID] banned $username[$findUser[$message[1]] for $message[2]].]
 $onlyBotPerms[ban;i need the {perms} permission to be able to do this.]
+$onlyPerms[ban;You must have the {perms} permission to use this command.]
+$onlyIf[$messsage[1]!=;Please mention or give an id of someone to ban]
 `
 })

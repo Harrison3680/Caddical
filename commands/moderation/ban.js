e4435006ebb49477739622e1fbdc6dc95112a5e6
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-04 22:51:06 -0500

    Update ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,5 +1,7 @@
 module.exports = ({
-name:"kick",
+name:"ban",
 code:`$ban[$findUser[$message[1]];$message[2]]
-$description[$username[$authorID] kicked $username[$findUser[$message[1]] for $message[2]]]`
+$description[$username[$authorID] banned $username[$findUser[$message[1]] for $message[2]].]
+$onlyBotPerms[ban;i need the {perms} permission to be able to do this.]
+`
 })

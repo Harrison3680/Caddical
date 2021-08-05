f815f32427564f729c2b545dc1ef7fd73bfd9a36
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-04 23:30:50 -0500

    Update mute.js

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -4,7 +4,7 @@ code: `$replyIn[$message[2]]
 $giveRole[$getServerVar[mutedroleid];$findUser[$message[1]];$messageSlice[>3]]
 :white_check_mark: | Gave the muted role to $username[$findUser[$message[1]]]!
 Reason: *$messageSlice[>3]*
-$onlyIf[$message[2]!=;You need to get a specified time example C!mute Harrison 3d Yes ]
+$onlyIf[$message[2]!=;You need to get a specified time example C!mute Harrison 3d Yes **MAX MUTE TIME IS 29 DAYS**]
 $onlyIf[$message[1]!=;You need to mention someone or give an id]
 $onlyIf[$getServerVar[mutedroleid]!=;You have to have a muted role set to set one use C!setmuted <roleID>]
 `

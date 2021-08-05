dfd5d66463a826d3bb372d87810d458a950e298b
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-05 18:13:22 -0500

    Update setmuted.js

--- commands/moderation/setmuted.js
+++ commands/moderation/setmuted.js
@@ -1,5 +1,5 @@
 module.exports = ({
-name:"setmute",
+name:"setmuted",
 code:`$setServerVar[mutedroleid;$message[1]]
 $onlyIf[$roleExists[$message[1];$guildID]==false;Please provide a valid role ID]`  
 })

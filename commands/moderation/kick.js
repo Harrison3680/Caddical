ba65fd91328c7d8ae8ad86d59b7517cb2a62fe89
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-04 22:51:50 -0500

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -1,5 +1,8 @@
 module.exports = ({
 name:"kick",
 code:`$kick[$findUser[$message[1]];$message[2]]
-$description[$username[$authorID] kicked $username[$findUser[$message[1]] for $message[2]]]`
+$description[$username[$authorID] kicked $username[$findUser[$message[1]] for $message[2]].]
+$onlyBotPerms[kick;i need the {perms} permission to be able to do this.]
+`
+`
 })

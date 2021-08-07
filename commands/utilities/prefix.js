5600aac2bc9ddcc1b874363f681b2acdd25ce560
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-07 14:19:34 -0500

    Update prefix.js

--- commands/utilities/prefix.js
+++ commands/utilities/prefix.js
@@ -4,4 +4,5 @@ code: `
 $setServerVar[prefix;$message[1]]
 I have set the prefix to **$message[1]**
 $onlyIf[$message[]!=;My prefix is \`$getServerVar[prefix]\`. You can change it by using **$getServerVar[prefix]prefix <new prefix>**]
-$onlyPerms[managemessages;You need \`Manage Messages\` to use that]
+$onlyPerms[managemessages;You need \`Manage Messages\` to use that]`
+})

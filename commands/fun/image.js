be091cbcef811184cbaf07fa42c7ccee4df48bba
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-07 18:53:33 -0500

    Create image.js

---
+++ commands/fun/image.js
@@ -0,0 +1,8 @@
+odule.exports = ({
+name: "image",
+code: `
+$image[https://api.alexflipnote.dev/supreme?text=$replaceText[$message[]; ;+]]
+$footer[Requested by $username[$authorID]#$discriminator[$authorID];$authorAvatar[webp;true;1024]]
+$color[$random[00000;99999]]
+$onlyIf[$message[]!=;Provide a text for me to convert to an image]`
+})

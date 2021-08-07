2117d33a0767320255c4f4593572dcf8ead93407
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-07 18:51:52 -0500

    Update twitter.js

--- commands/fun/twitter.js
+++ commands/fun/twitter.js
@@ -6,3 +6,4 @@ $color[$random[00000;99999]]
 $description[{hyper:Here are the results for your search!:https://twitter.com/search?q=$replaceText[$message[]; ;+]}]
 $footer[Twitter Search | $getServerVar[prefix]twitter;$authorAvatar[webp;true;1024]]
 $onlyIf[$message[]!=;**You must type something to search it.**]`
+})

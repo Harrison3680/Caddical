d093438316602a4d2ddde35aea24ac894238e8c1
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-07 18:39:21 -0500

    Update twitter.js

--- commands/fun/twitter.js
+++ commands/fun/twitter.js
@@ -5,4 +5,4 @@ $title[Twitter Search]
 $color[$random[00000;99999]]
 $description[{hyper:Here are the results for your search!:https://twitter.com/search?q=$replaceText[$message[]; ;+]}]
 $footer[Twitter Search | $getServerVar[prefix]twitter;$authorAvatar[webp;true;1024]]
-$onlyIf[$message[]!=;**You must type something to search it.**]
+$onlyIf[$message[]!=;**You must type something to search it.**]`

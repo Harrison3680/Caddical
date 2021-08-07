b5179af1bb2b384ab74610c54392b042b0211b45
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-07 18:51:17 -0500

    Update coin.js

--- commands/fun/coin.js
+++ commands/fun/coin.js
@@ -5,3 +5,4 @@ $title[Okay, I'm flipping]
 $description[and the coin lands on $randomText[||<:nodranicoin:717105076119928902>**Heads**||;||<:nodranicoin:717105076119928902>**Tails**||]]
 $color[$random[00000;999999]]
 `
+})

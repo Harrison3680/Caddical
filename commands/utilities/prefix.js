f22367ed982579dfd3e6c9abae1176ed24b2a045
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-07 13:11:45 -0500

    Create prefix.js

---
+++ commands/utilities/prefix.js
@@ -0,0 +1,7 @@
+module.exports = ({
+name: "prefix",
+code: `
+$setServerVar[prefix;$message[1]]
+I have set the prefix to **$message[1]**
+$onlyIf[$message[]!=;My prefix is \`$getServerVar[prefix]\`. You can change it by using **$getServerVar[prefix]prefix <new prefix>**]
+$onlyPerms[managemessages;You need \`Manage Messages\` to use that]

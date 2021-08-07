a6a561d13a3bd761b1a6d18b2bce163d75cdeb32
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-07 18:46:49 -0500

    Create coin.js

---
+++ commands/fun/coin.js
@@ -0,0 +1,7 @@
+module.exports = ({
+name: "coinflip",
+code: `
+$title[Okay, I'm flipping]
+$description[and the coin lands on $randomText[||<:nodranicoin:717105076119928902>**Heads**||;||<:nodranicoin:717105076119928902>**Tails**||]]
+$color[$random[00000;999999]]
+`

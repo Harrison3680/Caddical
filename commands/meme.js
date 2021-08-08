11d3981e645cf6d179790fa2b152c2eeea2aa153
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-07 19:10:08 -0500

    Delete meme.js

--- commands/meme.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({
-name: "meme",
-code: `
-$title[$api[https://meme-api.herokuapp.com/gimme;title]]
-$image[$api[https://meme-api.herokuapp.com/gimme;url]]
-$footer[Posted on r/$api[https://meme-api.herokuapp.com/gimme;subreddit] | $api[https://meme-api.herokuapp.com/gimme;ups] Upvotes]`
-})

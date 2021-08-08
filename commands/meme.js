a053ec75217bdb049bf3fe34ceee17ef0fe762fc
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-07 19:09:34 -0500

    Create meme.js

---
+++ commands/meme.js
@@ -0,0 +1,7 @@
+module.exports = ({
+name: "meme",
+code: `
+$title[$api[https://meme-api.herokuapp.com/gimme;title]]
+$image[$api[https://meme-api.herokuapp.com/gimme;url]]
+$footer[Posted on r/$api[https://meme-api.herokuapp.com/gimme;subreddit] | $api[https://meme-api.herokuapp.com/gimme;ups] Upvotes]`
+})

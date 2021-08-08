30355634d10159c119466affd7ada4b68558a2d7
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-08 08:26:44 -0500

    Update rps.js

--- commands/fun/rps.js
+++ commands/fun/rps.js
@@ -4,7 +4,7 @@ code: `
 $title[Rock, Paper, Scissors!]
 $description[
 $addField[$username[$authorID] Selected;$message[]] 
-$addField[Nodrani Selected;$randomText[Rock;Paper;Scissors!]]
+$addField[Caddical Selected;$randomText[Rock;Paper;Scissors!]]
 ]
 $footer[Rock Paper Scissors | nd!rps;$authorAvatar[webp;true;1024]]
 $color[$random[00000;99999]]

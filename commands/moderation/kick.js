64a91db5ea0f1e90d9fdb6ba7b718f5065b1aedb
Ricky <47152942+Ricky-js@users.noreply.github.com>
2021-08-19 11:55:41 -0400

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -1,3 +1,4 @@
+/*
 module.exports = ({
 name:"kick",
 code:`$kick[$findUser[$message[1]];$message[2]]
@@ -7,3 +8,22 @@ $onlyPerms[kick;You must have the {perms} permission to use this command.]
 $onlyIf[$messsage[1]!=;Please mention or give an id of someone to kick]
 `
 })
+*/
+
+
+let u = "$findUser[$message[1]]"
+
+module.exports = ({
+name:"kick",
+code: `
+$kick[${u};$guildID;(By: $tag[]) - $replaceText[$replaceText[$checkCondition[$message[2]==];true;No Reason Provided];false;$messageSlice[>1]]
+**$tag[${u}]** has been kicked by **$tag[]**
+$onlyIf[$rolePosition[$highestRole]<$rolePosition[$highestRole[${u}]];You cannot kick a higher ranked user]
+$onlyIf[$rolePosition[$highestRole[$client[id]]]<$rolePosition[$highestRole[${u}]];My rank isn't high enough to kick that user]
+$onlyIf[$ownerID!=${u};You can't kick the owner]
+$onlyIf[$authorID!=${u};You can't kick yourself]
+$onlyIf[$client[id]!=${u};Rude :(]
+$onlyIf[$userExists[${u}]==true;That user doesn't appear to exist]
+$onlyBotPerms[kick;:x: I'm missing the \`{perms}\` permission!]
+$onlyPerms[kick;:x: You're missing the \`{perms}\` permissions!]
+`})

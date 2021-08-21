d8b7e20c6ab8c8509a50014c595f5262818efb18
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-21 18:50:17 -0500

    Update coin.js

--- commands/fun/coin.js
+++ commands/fun/coin.js
@@ -2,7 +2,7 @@ module.exports = ({
 name: "coinflip",
 code: `
 $title[Okay, I'm flipping]
-$description[and the coin lands on $randomText[||<:nodranicoin:717105076119928902>**Heads**||;||<:nodranicoin:717105076119928902>**Tails**||]]
+$description[and the coin lands on $randomText[||**Heads**||;||**Tails**||]]
 $color[$random[00000;999999]]
 `
 })

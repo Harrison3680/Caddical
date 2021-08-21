73d4a162dd76788f94752866efb7e8b9f54ddd51
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-21 10:23:49 -0500

    Update eval.js

--- commands/developer/eval.js
+++ commands/developer/eval.js
@@ -1,5 +1,5 @@
 module.exports = ({
 name:"eval",
 code:`$eval[$message[]]
-$onlyForUsers[672215920558604332;634860829132455937;NO]`  
+$onlyForUsers[672215920558604332;634860829132455937;622146791659405313;NO]`  
 })

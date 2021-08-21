5abb74576711acbc9f1fd1f39f201b3a29b7c4f4
DinoTheDev <77805841+DinoTheDevOfficial@users.noreply.github.com>
2021-08-20 20:50:00 -0700

    added code to lockdown

--- commands/moderation/lockdown.js
+++ commands/moderation/lockdown.js
@@ -1,1 +1,10 @@
-// temp
+module.exports = {
+  name: "lockdown",
+  code: `
+  $title[Lockdown]
+  $description[$username issued a lockdown]
+  $modifyChannelPerms[$channelID;-sendmessages;$guildID]
+  $onlyBotPerms[managechannels;:x: I'm missing the \`{perms}\` permission!]
+  $onlyPerms[managechannels;:x: You're missing the \`{perms}\` permissions!]
+  `
+}

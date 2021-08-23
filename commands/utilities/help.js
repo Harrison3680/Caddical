226f10b8c53ad41a3a1ed9fd86015486bf94c0d4
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-22 19:17:54 -0500

    Update help.js

--- commands/utilities/help.js
+++ commands/utilities/help.js
@@ -1,4 +1,8 @@
 module.exports = {
 name:"help",
-code:`hi`
+code:`
+$onlyIf[$message[1]!=util;{execute:util}]
+$onlyIf[$message[1]!=fun;{execute:fun}]
+$onlyIf[$message[1]!=mod;{execute:mod}]
+$onlyIf[$message[1]!=dev;{execute:dev}]`
 }

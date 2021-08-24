4a1484576122baec46a0020dbfd50f026da837be
Ricky <ricky.dana2006@gmail.com>
2021-08-24 15:10:01 -0400

    Update leveler.js

--- events/leveling/leveler.js
+++ events/leveling/leveler.js
@@ -1,10 +1,9 @@
 module.exports.SpaceCommand = ({
 name: "leveler",
 code:`
-$replaceText[$replaceText[$replaceText[$replaceText[$getServerVar[rmsg];{user.tag};$tag[];{user.mention};<@$authorID>];{level};$getUserVar[lvl]];{exp};$getUserVar[exp]]
+$replaceText[$replaceText[$replaceText[$replaceText[$getServerVar[rmsg];{user.tag};$tag[]];{user.mention};<@$authorID>];{level};$getUserVar[lvl]];{exp};$getUserVar[exp]]
 $useChannel[$getServerVar[rch]]
 $setUserVar[lvl;$sum[$getUserVar[lvl];1]]
 $setUserVar[rexp;$multi[$getUserVar[rexp];$getUserVar[lvl]]]
 $onlyIf[$getUserVar[exp]>=$getUserVar[rexp];]
-$onlyIf[$guildID!=;] 
 `})

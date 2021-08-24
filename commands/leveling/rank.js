bacafdf750ecbdb3a1f7994ec2ef28560b3aa90b
Ricky <ricky.dana2006@gmail.com>
2021-08-24 14:09:45 -0400

    Update rank.js

--- commands/leveling/rank.js
+++ commands/leveling/rank.js
@@ -1,4 +1,4 @@
-let uri = 'https://vacefron.nl/api/rankcard?username=$username[$mentioned[1;yes]]&avatar=$userAvatar[$mentioned[1;yes]]?size=4096&level=$getUserVar[lvl;$mentioned[1;yes]]&rank=&currentxp=$getUserVar[exp;$mentioned[1;yes]]&nextlevelxp=$getUserVar[rexp;$mentioned[1;yes]]&previouslevelxp=0&custombg=https://cdn.discordapp.com/attachments/793071150614970388/794565647760752650/20210101_205624.jpg&xpcolor=ffffff&isboosting=true'
+let uri = 'https://vacefron.nl/api/rankcard?username=$username[$mentioned[1;yes]]&avatar=$userAvatar[$mentioned[1;yes]]?size=4096&level=$getUserVar[lvl;$mentioned[1;yes]]&rank=&currentxp=$getUserVar[exp;$mentioned[1;yes]]&nextlevelxp=$getUserVar[rexp;$mentioned[1;yes]]&previouslevelxp=0&custombg=https://cdn.discordapp.com/attachments/793071150614970388/794565647760752650/20210101_205624.jpg&xpcolor=ffffff&isboosting=$isBoosting[$authorID]'
 
 module.exports = {
 name:"rank",

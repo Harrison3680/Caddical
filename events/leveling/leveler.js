90d6f174faedf42f9d56e63de2f03baaf17fbefa
Ricky <ricky.dana2006@gmail.com>
2021-08-24 15:28:01 -0400

    Update leveler.js

--- events/leveling/leveler.js
+++ events/leveling/leveler.js
@@ -1,7 +1,7 @@
 module.exports.SpaceCommand = ({
 name: "leveler",
 code:`
-$replaceText[$replaceText[$replaceText[$replaceText[$getServerVar[rmsg];{user.tag};$tag[]];{user.mention};<@$authorID>];{level};$getUserVar[lvl]];{exp};$getUserVar[exp]]
+$replaceText[$replaceText[$replaceText[$replaceText[$getServerVar[rmsg];{user.tag};$tag];{user.mention};<@$authorID>];{level};$getUserVar[lvl]];{exp};$getUserVar[exp]]
 $useChannel[$getServerVar[rch]]
 $setUserVar[lvl;$sum[$getUserVar[lvl];1]]
 $setUserVar[rexp;$multi[$getUserVar[rexp];$getUserVar[lvl]]]

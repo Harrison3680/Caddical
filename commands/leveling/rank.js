47e018e9de827ca728652f7426d63440ee73c21c
Ricky <ricky.dana2006@gmail.com>
2021-08-24 14:20:03 -0400

    Update rank.js

--- commands/leveling/rank.js
+++ commands/leveling/rank.js
@@ -1,7 +1,7 @@
-let uri = 'https://vacefron.nl/api/rankcard?username=$username[$mentioned[1;yes]]&avatar=$userAvatar[$mentioned[1;yes]]?size=4096&level=$getUserVar[lvl;$mentioned[1;yes]]&rank=&currentxp=$getUserVar[exp;$mentioned[1;yes]]&nextlevelxp=$getUserVar[rexp;$mentioned[1;yes]]&previouslevelxp=0&custombg=https://cdn.discordapp.com/attachments/793071150614970388/794565647760752650/20210101_205624.jpg&xpcolor=ffffff&isboosting=$isBoosting[$authorID]'
-
 module.exports = {
 name:"rank",
 code: `
-${url}
+$author[Reee]
+$color[]
+$image[https://vacefron.nl/api/rankcard?username=$username[$mentioned[1;yes]]&avatar=$userAvatar[$mentioned[1;yes]]?size=4096&level=$getUserVar[lvl;$mentioned[1;yes]]&rank=&currentxp=$getUserVar[exp;$mentioned[1;yes]]&nextlevelxp=$getUserVar[rexp;$mentioned[1;yes]]&previouslevelxp=0&custombg=https://cdn.discordapp.com/attachments/793071150614970388/794565647760752650/20210101_205624.jpg&xpcolor=ffffff&isboosting=$isBoosting[$authorID]]
 `}

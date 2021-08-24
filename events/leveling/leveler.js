a51a5bd44f408929a3e2bcaeffc90dc44f6beeae
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-24 13:50:30 -0500

    Update leveler.js

--- events/leveling/leveler.js
+++ events/leveling/leveler.js
@@ -4,7 +4,7 @@ code:`
 $replaceText[$replaceText[$replaceText[$replaceText[$getServerVar[rmsg];{user.tag};$tag[];{user.mention};<@$authorID>];{level};$getUserVar[lvl]];{exp};$getUserVar[exp]]
 $useChannel[$getServerVar[rch]]
 $setUserVar[lvl;$sum[$getUserVar[lvl];1]]
-$setUserVar[rexp;$multi[$getUserVar[rexp];2]]
+$setUserVar[rexp;$multi[$getUserVar[rexp];$getUserVar[lvl]]]
 $onlyIf[$getUserVar[exp]>=$getUserVar[rexp];]
 $onlyIf[$guildID!=;] 
 `})

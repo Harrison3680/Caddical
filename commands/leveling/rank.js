59f3abf26430d0dbfe6df0139fff9abdab596921
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-24 12:50:28 -0500

    Update rank.js

--- commands/leveling/rank.js
+++ commands/leveling/rank.js
@@ -1,4 +1,4 @@
 module.exports = {
 name:"rank",
-code: `$image[https://vacefron.nl/api/rankcard?username=$replaceText[$username[$mentioned[1;yes]]; ;+;-1]&avatar=$userAvatar[$mentioned[1;yes]]?size=4096&level=$getUserVar[lvl;$mentioned[1;yes]]&rank=&currentxp=$getUserVar[exp;$mentioned[1;yes]]&nextlevelxp=$getUserVar[rexp;$mentioned[1;yes]]&previouslevelxp=0&custombg=https://cdn.discordapp.com/attachments/793071150614970388/794565647760752650/20210101_205624.jpg&xpcolor=ffffff&isboosting=true]`  
+code: `$image[https://vacefron.nl/api/rankcard?username=$username[$mentioned[1;yes]&avatar=$userAvatar[$mentioned[1;yes]]?size=4096&level=$getUserVar[lvl;$mentioned[1;yes]]&rank=&currentxp=$getUserVar[exp;$mentioned[1;yes]]&nextlevelxp=$getUserVar[rexp;$mentioned[1;yes]]&previouslevelxp=0&custombg=https://cdn.discordapp.com/attachments/793071150614970388/794565647760752650/20210101_205624.jpg&xpcolor=ffffff&isboosting=true]`  
 }

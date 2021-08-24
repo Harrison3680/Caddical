3c617f85d3af12ee14b3d683d26283bf9d723eff
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-24 10:56:58 -0500

    Create leveler.js

---
+++ events/leveling/leveler.js
@@ -0,0 +1,10 @@
+module.exports.SpaceCommand = ({
+name: "leveler",
+code:`
+$replaceText[$replaceText[$replaceText[$replaceText[$getServerVar[rmsg];{user.tag};$tag[];{user.mention};<@$authorID>];{level};$getUserVar[lvl]];{exp};$getUserVar[exp]]
+$useChannel[$getServerVar[rch]]
+$setUserVar[lvl;$sum[$getUserVar[lvl];1]]
+$setUserVar[rexp;$multi[$getUserVar[rexp];2]]
+$onlyIf[$getUserVar[exp]>=$getUserVar[rexp];]
+$onlyIf[$guildID!=;]` 
+`})

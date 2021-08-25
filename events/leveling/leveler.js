0654a6493572fe6c39b3784ccadf288faa6361c3
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-24 20:26:47 -0500

    Update leveler.js

--- events/leveling/leveler.js
+++ events/leveling/leveler.js
@@ -1,7 +1,7 @@
 module.exports.SpaceCommand = ({
 name: "leveler",
 code:`
-$replaceText[$replaceText[$replaceText[$replaceText[$getServerVar[rmsg];{user.tag};$tag];{user.mention};<@$authorID>];{level};$getUserVar[lvl]];{exp};$getUserVar[exp]]
+<@$authorID> has leveled up to $getUserVar[lvl]
 $useChannel[$getServerVar[rch]]
 $setUserVar[lvl;$sum[$getUserVar[lvl];1]]
 $setUserVar[rexp;$multi[$getUserVar[rexp];$getUserVar[lvl]]]

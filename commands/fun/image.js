9809c290a6d2cf4c49bb7cc1c56dfc458f3f319b
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-27 07:49:39 -0500

    Delete image.js

--- commands/fun/image.js
+++
@@ -1,8 +0,0 @@
-module.exports = ({
-name: "image",
-code: `
-$image[https://api.alexflipnote.dev/supreme?text=$replaceText[$message[]; ;+]]
-$footer[Requested by $username[$authorID]#$discriminator[$authorID];$authorAvatar[webp;true;1024]]
-$color[$random[00000;99999]]
-$onlyIf[$message[]!=;Provide a text for me to convert to an image]`
-})

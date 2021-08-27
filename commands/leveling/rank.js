1214b4d563724843a87d480071322d925a004051
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-27 00:11:02 -0500

    Update rank.js

--- commands/leveling/rank.js
+++ commands/leveling/rank.js
@@ -3,5 +3,5 @@ name:"rank",
 code: `
 $title[$username[$mentioned[1;yes]]'s rank]
 $color[]
-$image[https://vacefron.nl/api/rankcard?username=$username[$mentioned[1;yes]]&avatar=$userAvatar[$mentioned[1;yes]]?size=4096&level=$getUserVar[lvl;$mentioned[1;yes]]&rank=&currentxp=$getUserVar[exp;$mentioned[1;yes]]&nextlevelxp=$getUserVar[rexp;$mentioned[1;yes]]&previouslevelxp=0&custombg=$getUserVar[lvlbg]&xpcolor=FF0000&isboosting=$isBoosting[$authorID]]
+$image[https://vacefron.nl/api/rankcard?username=$username[$mentioned[1;yes]]&avatar=$userAvatar[$mentioned[1;yes]]?size=4096&level=$getUserVar[lvl;$mentioned[1;yes]]&rank=&currentxp=$getUserVar[exp;$mentioned[1;yes]]&nextlevelxp=$getUserVar[rexp;$mentioned[1;yes]]&previouslevelxp=0&custombg=$getUserVar[lvlbg]&xpcolor=$getUserVar[xpcolor]&isboosting=$isBoosting[$authorID]]
 `}

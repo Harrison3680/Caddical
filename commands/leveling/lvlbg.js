d0705d8465a13b89df09446f3ae9ab35f57043aa
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-26 23:55:59 -0500

    Update lvlbg.js

--- commands/leveling/lvlbg.js
+++ commands/leveling/lvlbg.js
@@ -1,5 +1,5 @@
 module.exports = ({
-name:"lvlbg",
+name:"set-bg",
 code:`$setUserVar[lvlbg;$message[1];$authorID]
 $onlyIf[$isValidImageURL[$message[1]]!=false;You must provide a valid image url]`  
 })

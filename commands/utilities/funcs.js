4edc146c2d7511ba3b78ff07617e20ee8e35f222
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-28 09:47:29 -0500

    Update funcs.js

--- commands/utilities/funcs.js
+++ commands/utilities/funcs.js
@@ -2,12 +2,10 @@ module.exports = {
     name: "docs",
     description: "Official Documentation command for functions",
     code: `
-\`\`\`
-$help[$toLowercase[$message[1]];usage]
-\`\`\`
 $author[Type - $help[$toLowercase[$message[1]];type];$authorAvatar]
 $title[$help[$toLowercase[$message[1]];name]]
 $addField[__Description:__;$help[$toLowercase[$message[1]];description]]
+$addField[__Usage__;$help[$toLowercase[$message[1]];usage]]
 $footer[Here ya go.]
 $color[RANDOM]
 $onlyIf[$checkCondition[$help[$toLowercase[$message[1]];name]==invalidFunction]!=true;:x: \`$message[]\` is invalid! Try again.]

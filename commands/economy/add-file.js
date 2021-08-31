a94f4d0c7aa3bbb866e4909cf6bdbca3292e2a40
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-31 13:35:08 -0500

    Create add-file.js

---
+++ commands/economy/add-file.js
@@ -0,0 +1,6 @@
+module.exports = ({
+name:"add-file",
+code:`$description[1 file has been addded to the vault]
+$setServerVar[files;$sum[$getServerVar[files];1]]
+$onlyIf[$getServerVar[files]==]`  
+})

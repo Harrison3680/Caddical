99763399d61109e40f15c80dd25a15f1dbf5674e
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-31 15:28:25 -0500

    Update heist.js

--- commands/economy/heist.js
+++ commands/economy/heist.js
@@ -1,8 +1,8 @@
 module.exports = ({
 name:"heist",
-code:`$editIn[1m;{description:You successfully robbed the vault for $multi[$getServerVar[files];10]}]
+code:`$setServerVar[files;$sub[$getServerVar[files];$getServerVar[files]]]
+$editIn[1m;{description:You successfully robbed the vault for $multi[$getServerVar[files];10]}]
 $setVar[cash;$sum[$getVar[cash;$authorID];$multi[$getServerVar[files];10]];$authorID]
-$setServerVar[files;$sub[$getServerVar[files];$getServerVar[files]]]
 $replyIn[1m]
 $description[Raiding the vault wait for a few minutes]
 $setServerVar[C4;false]

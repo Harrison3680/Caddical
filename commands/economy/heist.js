38bcb6eab1341f1a87d8c90417e952af62e34efb
Ricky-js <ricky.dana2006@gmail.com>
2021-08-31 14:45:10 -0400

    Update heist.js

--- commands/economy/heist.js
+++ commands/economy/heist.js
@@ -3,6 +3,7 @@ name:"heist",
 code:`$editIn[1m;{description:You successfully robbed the vault for $multi[$getServerVar[files];10]}]
 $setVar[cash;$sum[$getVar[cash;$authorID];$multi[$getServerVar[files];10];$authorID]
 $setServerVar[files;$sub[$getServerVar[files];$getServerVar[files]]]
+$replyIn[1m]
 $description[Raiding the vault wait for a few minutes]
 $setServerVar[C4;false]
 $onlyIf[$getServerVar[C4]==true;you need C4 to blow open the vault]

ad8a10b397c064fb7af85ec7ccdb033905136ab5
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-31 00:45:18 -0500

    Create rob.js

---
+++ commands/economy/rob.js
@@ -0,0 +1,9 @@
+module.exports = ({
+name:"rob",
+code:`$title[Rob Successful]
+$description[Succefully Robbed $username[$findUser[$message[1]]]]
+$setVar[cash;$sum[$getVar[cash;$authorID];$random[100;350]];$authorID]
+$setVar[cash;$sub[$getVar[cash;$findUser[$message[1]];$random[150;350]];$findUser[$message[1]]]
+$onlyIf[$getVar[cash;$findUser[$message[1]]]>=$random[150;350];You cannot rob that person as they are poor]
+$onlyIf[$findUser[$message[1]]==$authorID;Why... would you rob yourself?..]` 
+})

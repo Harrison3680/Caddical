bf616e6d24fe34e2e6c1e90f2a525055e6166b9b
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-09-01 20:14:42 -0500

    Update degrees.js

--- commands/education/degrees.js
+++ commands/education/degrees.js
@@ -1,7 +1,7 @@
 module.exports = ({
 name:"degree",
-code:`$if[$message[1]==associate;$description[A associate degree is used to work low class jobs]]
-$if[$message[1]==bachlor;$description[A bachlor degree is used to work medium class jobs]]
-$if[$message[1]==master;$description[A master degree is used to work high class jobs]]
-$if[$message[1]==docterate;$description[A docterate degree is used to work very high class jobs]]`  
+code:`$if[$message[1]==associate;A associate degree is used to work low class jobs]
+$if[$message[1]==bachlor;A bachlor degree is used to work medium class jobs]
+$if[$message[1]==master;A master degree is used to work high class jobs]
+$if[$message[1]==docterate;A docterate degree is used to work very high class jobs]`  
 }) 

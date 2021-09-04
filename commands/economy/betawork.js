718e3e4f02eaf73b45391891c1967fbd58688964
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-09-04 01:06:01 -0500

    Create betawork.js

---
+++ commands/economy/betawork.js
@@ -0,0 +1,4 @@
+module.exports = ({
+  name:"bwork",
+  code:`$if[$message[1]==youtuber;$if[$getVar[youtuber]!=false;You worked as a youtuber and earn $random[100;250] $onlyIf[$getVar[diploma;$authorID]!=false]]`
+})

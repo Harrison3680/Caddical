b4fe37b0dfb38e1e3fcb13452e2fea8429f3ab56
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-09-04 09:59:54 -0500

    Update betawork.js

--- commands/economy/betawork.js
+++ commands/economy/betawork.js
@@ -1,4 +1,4 @@
 module.exports = ({
   name:"bwork",
-  code:`$If[$and[$message[1]==youtuber;$getVar[job;$authorID]==youtuber]==true;$setVar[cash;$random[100;250];$authorID] You worked as a youtuber and earned $$random[100;250] $onlyIf[$getVar[youtuber;$authorID]!=false] $onlyIf[$getVar[diploma;$authorID]!=false;You need a highschool diploma to do this.]]`
+  code:`$If[$and[$message[1]==youtuber;$getVar[job;$authorID]==youtuber]==true;$setVar[cash;$random[100;250];$authorID] You worked as a youtuber and earned $$random[100;250] $onlyIf[$getVar[youtuber;$authorID]!=false;You need to sign up as a youtuber] $onlyIf[$getVar[diploma;$authorID]!=false;You need a highschool diploma to do this.]]`
 })

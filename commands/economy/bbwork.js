ff0d6119adebd1b768ef623d8f4e67caed3c0b16
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-09-05 12:27:08 -0500

    Update bbwork.js

--- commands/economy/bbwork.js
+++ commands/economy/bbwork.js
@@ -8,17 +8,24 @@ name:"bbwork",
   $onlyIf[$getVar[diploma;$authorID]!=false;You need a highschool diploma to work here]]
   
   $If[$message[1]==cashier;
-  $setUserVar[cash;$sum[$getUserVar[cash;$authorID];$random[20;45]];$authorID] You worked as a youtuber and earned $$random[20;45]
+  $setUserVar[cash;$sum[$getUserVar[cash;$authorID];$random[20;45]];$authorID] You worked as a cashier and earned $$random[20;45]
   $onlyIf[$getVar[job;$authorID]==cashier;
   $replaceText[$replaceText[$checkCondition[$getVar[job;$authorID]==N/A];true;You don't have a job];false;You're already working as a **$getVar[job;$authorID]**!]]
   $onlyIf[$getVar[cashier;$authorID]!=false;You need to sign up as a cashier]
   $onlyIf[$getVar[diploma;$authorID]!=false;You need a highschool diploma to work here]]
   
   $If[$message[1]==electrician;
-  $setUserVar[cash;$sum[$getUserVar[cash;$authorID];$random[20;45]];$authorID] You worked as a youtuber and earned $$random[20;45]
+  $setUserVar[cash;$sum[$getUserVar[cash;$authorID];$random[20;45]];$authorID] You worked as a electrician and earned $$random[20;45]
   $onlyIf[$getVar[job;$authorID]==electrician;
   $replaceText[$replaceText[$checkCondition[$getVar[job;$authorID]==N/A];true;You don't have a job];false;You're already working as a **$getVar[job;$authorID]**!]]
   $onlyIf[$getVar[electrician;$authorID]!=false;You need to sign up as a electrician]
+  $onlyIf[$getVar[diploma;$authorID]!=false;You need a highschool diploma to work here]]
+  
+  $If[$message[1]==baker;
+  $setUserVar[cash;$sum[$getUserVar[cash;$authorID];$random[20;45]];$authorID] You worked as a baker and earned $$random[20;45]
+  $onlyIf[$getVar[job;$authorID]==baker;
+  $replaceText[$replaceText[$checkCondition[$getVar[job;$authorID]==N/A];true;You don't have a job];false;You're already working as a **$getVar[job;$authorID]**!]]
+  $onlyIf[$getVar[baker;$authorID]!=false;You need to sign up as a baker]
   $onlyIf[$getVar[diploma;$authorID]!=false;You need a highschool diploma to work here]]
   `
 })

720d55ec984657cee500b4d6e04d02ea465b73a6
Ricky-js <ricky.dana2006@gmail.com>
2021-09-05 00:44:29 -0400

    Update betawork.js

--- commands/economy/betawork.js
+++ commands/economy/betawork.js
@@ -15,7 +15,7 @@ module.exports = ({
   $onlyIf[$getVar[cashier;$authorID]!=false;You need to sign up as a cashier]
   $onlyIf[$getVar[diploma;$authorID]!=false;You need a highschool diploma to work here.]]
   
-  $if[$message[1]==electircian;
+  $If[$message[1]==electrician;
   $setUserVar[cash;$sum[$getUserVar[cash;$authorID];$random[75;105]];$authorID] You worked as a electrician and earned $$random[75;105]
   $onlyIf[$getVar[job;$authorID]==electrician;
   $replaceText[$replaceText[$checkCondition[$getVar[job;$authorID]==N/A];true;You don't have a job];false;You're already working as a **$getVar[job;$authorID]**!]]
@@ -25,7 +25,7 @@ module.exports = ({
   $If[$message[1]==quit;
   You just quit your job as a **$get[job]**.
   $setVar[$getVar[job];false;$authorID]]
-  $let[job;$getVar[job]]
+  $let[job;$getVar[job;$authorID]]
   $onlyIf[$checkCondition[$getVar[job;$authorID]==N/A]!=true;You don't have a job so you can't quit]
 '`})
 // baker, teacher, professor, plumber, 

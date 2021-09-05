99281ed1c4ea68d338ffe196aea98b774c548f99
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-09-05 11:57:08 -0500

    Update bbwork.js

--- commands/economy/bbwork.js
+++ commands/economy/bbwork.js
@@ -7,11 +7,18 @@ name:"bbwork",
   $onlyIf[$getVar[youtuber;$authorID]!=false;You need to sign up as a youtuber]
   $onlyIf[$getVar[diploma;$authorID]!=false;You need a highschool diploma to work here]]
   
-  $If[$message[1]==youtuber;
+  $If[$message[1]==cashier;
   $setUserVar[cash;$sum[$getUserVar[cash;$authorID];$random[20;45]];$authorID] You worked as a youtuber and earned $$random[20;45]
-  $onlyIf[$getVar[job;$authorID]==youtuber;
+  $onlyIf[$getVar[job;$authorID]==cashier;
   $replaceText[$replaceText[$checkCondition[$getVar[job;$authorID]==N/A];true;You don't have a job];false;You're already working as a **$getVar[job;$authorID]**!]]
-  $onlyIf[$getVar[youtuber;$authorID]!=false;You need to sign up as a youtuber]
+  $onlyIf[$getVar[cashier;$authorID]!=false;You need to sign up as a cashier]
+  $onlyIf[$getVar[diploma;$authorID]!=false;You need a highschool diploma to work here]]
+  
+  $If[$message[1]==electrician;
+  $setUserVar[cash;$sum[$getUserVar[cash;$authorID];$random[20;45]];$authorID] You worked as a youtuber and earned $$random[20;45]
+  $onlyIf[$getVar[job;$authorID]==electrician;
+  $replaceText[$replaceText[$checkCondition[$getVar[job;$authorID]==N/A];true;You don't have a job];false;You're already working as a **$getVar[job;$authorID]**!]]
+  $onlyIf[$getVar[electrician;$authorID]!=false;You need to sign up as a electrician]
   $onlyIf[$getVar[diploma;$authorID]!=false;You need a highschool diploma to work here]]
   `
 })

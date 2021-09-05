417ad1440d89486f70248a8b25228ee79221ca32
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-09-05 11:33:44 -0500

    Create bbwork.js

---
+++ commands/economy/bbwork.js
@@ -0,0 +1,9 @@
+module.exports = ({
+name:"bbwork",
+  code:`$If[$message[1]==youtuber; 
+  $setUserVar[cash;$sum[$getUserVar[cash;$authorID];$random[20;45]];$authorID] You worked as a youtuber and earned $$random[20;45]
+  $onlyIf[$getVar[job;$authorID]==youtuber;
+  $replaceText[$replaceText[$checkCondition[$getVar[job;$authorID]==N/A];true;You don't have a job];false;You're already working as a **$getVar[job;$authorID]**!]
+  $onlyIf[$getVar[youtuber;$authorID]!=false;You need to sign up as a youtuber] 
+  $onlyIf[$getVar[diploma;$authorID]!=false;You need a highschool diploma to work here.]]`
+})
